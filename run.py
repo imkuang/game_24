2e88c6cd02936af3d5f528c67ed3eba98305af27
xirikm <xirikm@gmail.com>
2020-04-13 11:32:01 +0800

    更新游戏过程中的说明文字

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 
 def usage_print():
     print("************* 24点小游戏 By 夕日 *************")
-    print("使用说明：")
+    print("使用说明:")
     print("1. 输入 answer 查看当前题目的答案")
     print("2. 输入 stop 终止游戏")
     print("3. 直接输入算式即可回答题目，若判断无解请输入 nope")
@@ -18,34 +18,35 @@ def main():
     while True:
         cards = gen_cards()
         print("*********************************************")
-        print("题目：", cards)
+        print("题目:", cards)
 
         # 排序，查找标准答案
         cards.sort()
         standard_answer = answers[tuple(cards)]
 
-        user_input = input(": ")
+        user_input = input(">")
         user_input = user_input.strip()
         if user_input == cmd_list[0]:
+            print("游戏结束，欢迎下次再来使用!")
             sys.exit(0)
         elif user_input == cmd_list[1]:
-            print("标准答案：", standard_answer.replace("nope", "无解"))
+            print("标准答案:", standard_answer.replace("nope", "无解"))
         elif user_input == cmd_list[2]:
             if standard_answer == "nope":
-                print("回答正确！答案： 无解")
+                print("回答正确!", "无解")
             else:
-                print("回答错误！正确答案：", standard_answer)
+                print("回答错误! 正确答案:", standard_answer)
         else:
             if is_legal(user_input, tuple(cards)):
                 try:
-                    if abs(eval(user_input) - 24) < 1e-10:  # 避免除法产生小数导致结果不再为 int 型的情况
-                        print("回答正确！答案：", user_input)
+                    if abs(eval(user_input) - 24) < 1e-10:  # 避免除法产生小数导致结果不再为int型的情况
+                        print("回答正确!", user_input + "=24")
                     else:
-                        print("回答错误！答案：", standard_answer)
+                        print("回答错误! 正确答案:", standard_answer.replace("nope", "无解"))
                 except ArithmeticError:
-                    print("无效的算式！标准答案：", standard_answer.replace("nope", "无解"))
+                    print("无效的算式! 标准答案:", standard_answer.replace("nope", "无解"))
             else:
-                print("不合法的输入！标准答案：", standard_answer.replace("nope", "无解"))
+                print("不合法的输入! 标准答案:", standard_answer.replace("nope", "无解"))
 
 
 if __name__ == "__main__":
